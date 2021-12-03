d1e8cf045e34e38685d1711a5dab70f3d071da1b
Kedar Singh <deepawalekedar319@gmail.com>
2021-12-03 07:45:41 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,40 @@
+class DataTypes {
+	public static void main(String[] args) 	{
+		//int a = 10L;
+		int a = (byte)10;
+		int b = (short)10;
+		int c = (int)10;
+		byte b1 = (short)12;
+		short s1 = 10;
+		//byte b2 = s1;
+		//int i1 = 0L;
+		long l1 = 1234567890;
+		//long l2 = 123456789012;
+		long l2 = 12345678909L;
+		byte b2 = 10;
+		//char c1 = '';
+		//char c2 = '	 ';
+		String ss1 = "";
+		byte b12='c';
+		byte String = 12;
+		//byte _ = 12;
+		byte $ = 12;
+		//kedar;
+		System.out.println('a');
+	}
+
+
+
+	public static void m1(byte b){
+		System.out.println("Byte");
+	}
+	public static void m1(int b){
+		System.out.println("Int");
+	}
+	public static void m1(char b){
+		System.out.println("Char");
+		}
+		public static void m1(long b){
+		System.out.println("Long");
+	}
+}
