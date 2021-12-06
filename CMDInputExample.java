91bb0b5063ba719f73b49dc8bebb6e4ead80995e
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2021-12-06 14:27:05 +0530

    Add files via upload
    Command Line arguments

---
+++ CMDInputExample.java
@@ -0,0 +1,7 @@
+class CMDInputExample{
+   public static void main(String[] args){
+     int val1 = Integer.parseInt(args[0]);
+     int val2 = Integer.parseInt(args[1]);
+     System.out.println("Sum is :: " + val1+val2);		
+   }
+}
