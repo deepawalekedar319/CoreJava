5b948d9f54a3cf2592c7fe4a77c6c71bf51baec0
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-01-12 15:49:16 +0530

    Programming Elements in Java

---
+++ SampleTest.java
@@ -0,0 +1,20 @@
+class SampleTest 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Java Programming Elements :: ");
+		System.out.println("1 :: Documentation Section ");
+		System.out.println("2 :: Package Statement");
+		System.out.println("3 :: import statements ");
+		System.out.println("4 :: Interface");
+		System.out.println("5 :: Abstract Classes");
+		System.out.println("6 :: Concrete Classes");
+		System.out.println("7 :: Final Classes");
+		System.out.println("8 :: Enum");
+		System.out.println("9 :: Anotation");
+		System.out.println("10 :: Record");
+		System.out.println("11 :: Main method class");
+	}
+}
+
+// Commands to get the Source Code   javap ClassName
