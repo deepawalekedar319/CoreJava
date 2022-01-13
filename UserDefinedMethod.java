c4422c26ee4a81de09f97d4668abdf1d535073aa
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-01-13 12:52:48 +0530

    User Defined methods

---
+++ UserDefinedMethod.java
@@ -0,0 +1,11 @@
+class UserDefinedMethod 
+{
+	static void m1(){
+		System.out.println("m1()");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		m1();
+	}
+}
