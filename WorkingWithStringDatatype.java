95e8d84561c953da11aac7c126cb25d42ff6c03b
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-04-18 21:06:19 +0530

    String Data type

---
+++ WorkingWithStringDatatype.java
@@ -0,0 +1,13 @@
+public class WorkingWithStringDatatype {
+    public static void main(String[] args) {
+        System.out.println("String 1");
+        String s1 = "String 2";
+        System.out.println(s1);
+        String String = "String";
+        System.out.println(String);
+        String System = "String";
+        //System.out.println(System);
+        java.lang.System.out.println(System);
+        // System String = "String";
+    }    
+}
