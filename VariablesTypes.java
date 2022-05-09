e1e42b3535afcb9d7885b04c0fd8dee735431d91
D . Kedar Singh <67812887+deepawalekedar319@users.noreply.github.com>
2022-05-09 14:28:49 +0530

    Variables Types

---
+++ VariablesTypes.java
@@ -0,0 +1,17 @@
+public class VariablesTypes {
+    
+    // static variables :: Fields
+    static int a = 10;
+    static int b = 20;
+
+    // non-static variables :: Attributes, Properties, Instance 
+    int c = 30;
+    int d = 20;
+
+    public static void main(String[] args) {
+        
+        // Local Variables
+        int a = 20;
+
+    } // main
+}
